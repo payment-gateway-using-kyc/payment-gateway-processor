9cf94b0fb1b0da5a11e3d5e1ae318aaf0274f6b7
Md. Shakil Hossain <shakilhossainpavel@gmail.com>
2022-03-31 02:13:47 +0600

    basic structure

---
+++ src/main/java/com/paymentgateway/paymentgatewayprocessor/PaymentGatewayProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.paymentgateway.paymentgatewayprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentGatewayProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paymentgateway/paymentgatewayprocessor/PaymentGatewayProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentgateway.paymentgatewayprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
